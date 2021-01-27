3b84aaf55594ad3d388f1cc2ce84d37a54f4b2f0
Majid-Aghamohammad <majid199372@gmail.com>
2021-01-27 07:44:46 +0330

    end of part 11

--- try_django/products/models.py
+++ try_django/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title       = models.CharField(max_length=255)
+    description = models.TextField(blank= True , null=True)
+    price       = models.DecimalField(decimal_places=2 , max_digits=1000000)
+    summary     = models.TextField()
+    featured    = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.title 
+
+
