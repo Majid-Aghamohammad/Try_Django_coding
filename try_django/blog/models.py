1fffa6e68f7b1081b889322cd59e1faf503e58ac
Majid-Aghamohammad <majid199372@gmail.com>
2021-02-01 00:10:51 +0330

    end of part 24

--- try_django/blog/models.py
+++ try_django/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class BlogPost(models.Model):
     title   = models.CharField(max_length=255)
+    slug    = models.SlugField()
     content = models.TextField()
 
     def __str__(self):
