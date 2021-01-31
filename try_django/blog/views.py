1d9cb6fa273b5ed62e5a045d07ee8e983e827b67
Majid-Aghamohammad <majid199372@gmail.com>
2021-01-31 23:55:33 +0330

    end of 1:33

--- try_django/blog/views.py
+++ try_django/blog/views.py
@@ -6,7 +6,7 @@ from .models import BlogPost
 
 def blog_post_detail_page(request, post_id):
     # obj = BlogPost.objects.get(id=post_id)
-    get_object_or_404(BlogPost, id= post_id)
+    obj = get_object_or_404(BlogPost, id= post_id)
     template_name = 'blog_post_detail.html'
     context = {'blog_object': obj}
     return render(request, template_name, context)
