7148d8f64a38fb849aa393cbc2a704e34fc1aa83
Majid-Aghamohammad <majid199372@gmail.com>
2021-02-02 09:00:36 +0330

    end of part 38/2:45

--- try_django/blog/Create_Form.py
+++ try_django/blog/Create_Form.py
@@ -6,3 +6,12 @@ class CreatePostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
         fields = ['title', 'slug', 'content']
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get('title')
+        quryset = BlogPost.objects.filter(title=title)
+
+        if quryset.exists():
+            raise forms.ValidationError("this title already has exists \n please change your title")
+        
+        return title
